51499adebe438463a0f7869abb3af740c7462cc1
purwar1997 <shubhampurwar511@gmail.com>
2023-03-01 22:12:49 +0530

    added api docs for signup route

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yaml';
+import fs from 'fs';
 import router from './routes/routes.js';
 
 const app = express();
+const data = fs.readFileSync('./swagger.yaml', { encoding: 'utf-8' });
+const swaggerDocument = YAML.parse(data);
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
