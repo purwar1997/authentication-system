1bcaba2fc56e398bb8d6959ddbf97ddc9eb5f45a
purwar1997 <shubhampurwar511@gmail.com>
2023-03-02 20:45:07 +0530

    removed cors package

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import cookieParser from 'cookie-parser';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yaml';
 import fs from 'fs';
-import cors from 'cors';
 import router from './routes/routes.js';
 
 const app = express();
@@ -11,12 +10,10 @@ const app = express();
 const data = fs.readFileSync('./swagger.yaml', { encoding: 'utf-8' });
 const swaggerDocument = YAML.parse(data);
 const options = { customCss: '.swagger-ui .topbar { display: none }' };
-const corsOptions = { origin: 'https://authentication-system-gf5i.onrender.com/api-docs' };
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
-app.use(cors(corsOptions));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 export default app;
