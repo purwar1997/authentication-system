abd21654e8f34793d934f406ac689d923d34f0e5
purwar1997 <shubhampurwar511@gmail.com>
2023-03-03 15:39:36 +0530

    updated auth middleware logic

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,24 +12,28 @@ const auth = asyncHandler(async (req, res, next) => {
     (req.headers.authorization && req.headers.authorization.startsWith('Bearer'))
   ) {
     token = req.cookies.token || req.headers.authorization.split(' ')[1];
-
-    let decodedToken;
-
-    try {
-      decodedToken = JWT.verify(token, config.JWT_SECRET);
-    } catch (err) {
-      throw new CustomError('Token invalid or expired', 500);
-    }
-
-    const user = await User.findById(decodedToken.userId);
-
-    if (!user) {
-      throw new CustomError('User not found', 404);
-    }
-
-    res.user = user;
-    return next();
   }
+
+  if (!token) {
+    throw new CustomError('Token is missing', 500);
+  }
+
+  let decodedToken;
+
+  try {
+    decodedToken = JWT.verify(token, config.JWT_SECRET);
+  } catch (err) {
+    throw new CustomError('Token invalid or expired', 500);
+  }
+
+  const user = await User.findById(decodedToken.userId);
+
+  if (!user) {
+    throw new CustomError('User not found', 404);
+  }
+
+  res.user = user;
+  return next();
 });
 
 export default auth;
